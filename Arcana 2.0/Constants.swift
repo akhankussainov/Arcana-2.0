d62fec599c0c1eaa025623e727deb5152d187583
Akhan <akhankussainov@gmail.com>
2017-03-29 05:03:32 +0600

    Add files via upload

---
+++ Arcana 2.0/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Arcana 2.0
+//
+//  Created by Akhan Kussainov on 3/26/17.
+//  Copyright © 2017 Akhan Kussainov. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Color {
+    static let primaryColor = UIColor.lightGray
+}
---
+++ Arcana 2.0/LoginVC.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Arcana 2.0
+//
+//  Created by Akhan Kussainov on 3/13/17.
+//  Copyright © 2017 Akhan Kussainov. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("loginVC loaded...")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
